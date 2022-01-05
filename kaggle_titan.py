85f584ade8413f1f48995902d6217d5068598d14
Filipp Trubin <55filipp@gmail.com>
2022-01-05 16:58:16 -0500

    002

--- kaggle_titan.py
+++ kaggle_titan.py
@@ -1,19 +1,104 @@
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
 import pandas as pd
-from sklearn.tree import DecisionTreeRegressor
+from sklearn.model_selection import train_test_split
 
-melb_houses = pd.read_csv('data/melb_data.csv')
-melb_houses.dropna(axis=0)
+# Read the data
+X_full = pd.read_csv('data\\train.csv', index_col='Id')
+X_test_full = pd.read_csv('data\\test.csv', index_col='Id')
 
-y = melb_houses.Price
-melb_features = ['Rooms', 'Bathroom', 'Landsize', 'Lattitude', 'Longtitude']
-X = melb_houses.loc[:, ['Rooms', 'Bathroom', 'Landsize', 'Lattitude', 'Longtitude']]
+# Remove rows with missing target, separate target from predictors
+X_full.dropna(axis=0, subset=['SalePrice'], inplace=True)
+y = X_full.SalePrice
+X_full.drop(['SalePrice'], axis=1, inplace=True)
 
-melbourne_model = DecisionTreeRegressor(random_state=1)
-melbourne_model.fit(X, y)
+# Break off validation set from training data
+X_train_full, X_valid_full, y_train, y_valid = train_test_split(X_full, y,
+                                                                train_size=0.8, test_size=0.2,
+                                                                random_state=0)
 
-iowa_houses = pd.read_csv('data/iowa_houses.csv')
-y = iowa_houses.Price
-X = iowa_houses.loc[:, ['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd']]
+# "Cardinality" means the number of unique values in a column
+# Select categorical columns with relatively low cardinality (convenient but arbitrary)
+categorical_cols = [cname for cname in X_train_full.columns if
+                    X_train_full[cname].nunique() < 10 and
+                    X_train_full[cname].dtype == "object"]
 
+# Select numerical columns
+numerical_cols = [cname for cname in X_train_full.columns if
+                  X_train_full[cname].dtype in ['int64', 'float64']]
 
-print(melbourne_model.predict(X.head()))
+# Keep selected columns only
+my_cols = categorical_cols + numerical_cols
+X_train = X_train_full[my_cols].copy()
+X_valid = X_valid_full[my_cols].copy()
+X_test = X_test_full[my_cols].copy()
+
+# Preprocessing for numerical data
+numerical_transformer = SimpleImputer(strategy='constant')
+
+# Preprocessing for categorical data
+categorical_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='most_frequent')),
+    ('onehot', OneHotEncoder(handle_unknown='ignore'))
+])
+
+# Bundle preprocessing for numerical and categorical data
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('num', numerical_transformer, numerical_cols),
+        ('cat', categorical_transformer, categorical_cols)
+    ])
+
+# Define model
+model = RandomForestRegressor(n_estimators=100, random_state=0)
+
+# Bundle preprocessing and modeling code in a pipeline
+clf = Pipeline(steps=[('preprocessor', preprocessor),
+                      ('model', model)
+                      ])
+
+# Preprocessing of training data, fit model
+clf.fit(X_train, y_train)
+
+# Preprocessing of validation data, get predictions
+preds = clf.predict(X_valid)
+
+print('MAE:', mean_absolute_error(y_valid, preds))
+
+# Preprocessing for numerical data
+numerical_transformer = SimpleImputer(strategy='constant')  # Your code here
+
+# Preprocessing for categorical data
+categorical_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='constant')),
+    ('onehot', OneHotEncoder(handle_unknown='ignore'))
+])  # Your code here
+
+# Bundle preprocessing for numerical and categorical data
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('num', numerical_transformer, numerical_cols),
+        ('cat', categorical_transformer, categorical_cols)
+    ])
+
+# Define model
+model = RandomForestRegressor(n_estimators=100, random_state=0)  # Your code here
+
+# Bundle preprocessing and modeling code in a pipeline
+my_pipeline = Pipeline(steps=[('preprocessor', preprocessor),
+                              ('model', model)
+                              ])
+
+# Preprocessing of training data, fit model
+my_pipeline.fit(X_train, y_train)
+
+# Preprocessing of validation data, get predictions
+preds = my_pipeline.predict(X_valid)
+
+# Evaluate the model
+score = mean_absolute_error(y_valid, preds)
+print('MAE:', score)
--- scratch_1.py
+++ scratch_1.py
@@ -1,84 +1,24 @@
-import matplotlib
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-from pandas.plotting import register_matplotlib_converters
-import statsmodels.api as sm
-from sklearn.preprocessing import MinMaxScaler
-from statsmodels.tsa.stattools import adfuller
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
+from xgboost import XGBRegressor
 
-print('oi')
+# Read the data
+data = pd.read_csv('data///melb_data.csv')
 
-pd.set_option('display.max_columns', 20)
+# Select subset of predictors
+cols_to_use = ['Rooms', 'Distance', 'Landsize', 'BuildingArea', 'YearBuilt']
+X = data[cols_to_use]
 
-register_matplotlib_converters()
-# %matplotlib inline
+# Select target
+y = data.Price
 
-dataset = pd.read_csv('AAPL.csv')
-dataset['Mean'] = (dataset['Low'] + dataset['High']) / 2
+# Separate data into training and validation sets
+X_train, X_valid, y_train, y_valid = train_test_split(X, y)
 
-steps = -1
-dataset_for_prediction = dataset.copy()
-dataset_for_prediction['Actual'] = dataset_for_prediction['Mean'].shift(steps)
-dataset_for_prediction = dataset_for_prediction.dropna()
-dataset_for_prediction['Date'] = pd.to_datetime(dataset_for_prediction['Date'])
-dataset_for_prediction.index = dataset_for_prediction['Date']
-
-sc_in = MinMaxScaler(feature_range=(0, 1))
-scaled_input = sc_in.fit_transform(
-    dataset_for_prediction[['Low', 'High', 'Open', 'Close', 'Volume', 'Adj Close', 'Mean']])
-scaled_input = pd.DataFrame(scaled_input)
-X = scaled_input
-
-sc_out = MinMaxScaler(feature_range=(0, 1))
-scaler_output = sc_out.fit_transform(dataset_for_prediction[['Actual']])
-scaler_output = pd.DataFrame(scaler_output)
-y = scaler_output
-
-X.rename(columns={0: 'Low', 1: 'High', 2: 'Open', 3: 'Close', 4: 'Volume', 5: 'Adj Close', 6: 'Mean'}, inplace=True)
-y.rename(columns={0: 'Stock Price next day'}, inplace=True)
-y.index = dataset_for_prediction.index
-
-# SPLITTING TO TRAINING / TEST
-train_size = int(len(dataset) * 0.7)
-test_size = int(len(dataset)) - train_size
-train_X, train_y = X[:train_size].dropna(), y[:train_size].dropna()
-test_X, test_y = X[train_size:].dropna(), y[train_size:].dropna()
-
-#   DECOMPOSITION FOR TREND / SEASONALITY / NOISE
-seas_d = sm.tsa.seasonal_decompose(X['Mean'], model='add', period=365)
-fig = seas_d.plot()
-fig.set_figheight(10)
-fig.set_figwidth(10)
-
-
-# Augmented Dickey-Fuller test
-def test_adf(series, title=''):
-    dfout = {}
-    dftest = sm.tsa.adfuller(series.dropna(), autolag='AIC', regression='ct')
-    for key, val in dftest[4].items():
-        dfout[f'critical value ({key})'] = val
-    if dftest[1] <= 0.05:
-        print("Strong evidence against Null Hypothesis")
-        print("Reject Null Hypothesis - Data is Stationary")
-        print("Data is Stationary", title)
-    else:
-        print("Strong evidence for  Null Hypothesis")
-        print("Accept Null Hypothesis - Data is not Stationary")
-        print("Data is NOT Stationary for", title)
-
-
-y_test = y['Stock Price next day'][:train_size].dropna()
-test_adf(y_test, " Stock Price")
-
-test_adf(y_test.diff(), 'Stock Price')
-
-# ACF / PACF
-fig, ax = plt.subplots(2, 1, figsize=(10, 5))
-fig = sm.tsa.graphics.plot_acf(y_test, lags=50, ax=ax[0])
-fig = sm.tsa.graphics.plot_pacf(y_test, lags=50, ax=ax[1])
-plt.show()
-
-print(y.head(5))
-pass
+model = RandomForestRegressor(random_state=0)
+model.fit(X_train, y)
+preds = model.predict(X_valid)
+score = mean_absolute_error(y_valid, preds)
+print('MAE:', score)
